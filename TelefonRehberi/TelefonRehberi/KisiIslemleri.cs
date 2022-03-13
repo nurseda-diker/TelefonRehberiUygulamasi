64c54c92265cc2471c9dadd2d0d471d6e26cf327
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2022-03-13 15:42:58 +0300

    Add files via upload

---
+++ TelefonRehberi/TelefonRehberi/KisiIslemleri.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonRehberi
+{
+    class KisiIslemleri
+    {
+
+       public Dictionary<string, long> telefonRehberi = new();
+
+       public void KayitEkle()
+        {
+            Console.WriteLine("Kaydetmek istediğiniz kisinin isim ve soyismini giriniz:");
+            string adSoyad = Console.ReadLine();
+            Console.WriteLine("Telefon numarasını giriniz:");
+            long telefonNo = long.Parse(Console.ReadLine());
+
+            if (telefonRehberi.ContainsKey(adSoyad))
+            {
+                Console.WriteLine("Bu kisi rehberde kayitli\n.Menuye donmek icin bir tusa basin.");
+            }
+            else
+            {
+                telefonRehberi.Add(adSoyad, telefonNo);
+                Console.WriteLine("Kayıt işlemi başarılı.\nMenuye donmek için bir tuşa basın.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Program.Menu();
+        }
+
+
+        public void NumaraSil()
+        {
+            Console.WriteLine(" Lütfen numarasını silmek istediğiniz kişinin adını ve soyadını giriniz:");
+            string input = Console.ReadLine();
+            if (telefonRehberi.ContainsKey(input))
+            {
+                Console.WriteLine("{0} isimli kişi rehberden silinmek üzere, onaylıyor musunuz ?(y/n)",input);
+                string cevap = Console.ReadLine();
+                if (cevap == "y")
+                {
+                    telefonRehberi.Remove(input);
+                    Console.WriteLine("Silme işlemi başarılı.\nMenuye dönmek için bir tuşa basın.");
+                }
+                else if (cevap == "n")
+                {
+                    Console.WriteLine("Silme işlemi iptal edildi.\nMenuye donmek için bir tuşa basın.");
+                    MenuyeDon();
+                }
+                MenuyeDon();
+
+            }
+            else
+            {
+                Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.\n" +
+                            "* Silmeyi sonlandırmak için : (1)\n" +
+                            "* Yeniden denemek için      : (2)");
+                int cevap = int.Parse(Console.ReadLine());
+                switch (cevap)
+                {
+                    case 1:
+                        Console.Clear();
+                        Program.Menu();
+                        break;
+                    case 2:
+                        NumaraSil();
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış sayı girişi.");
+                        MenuyeDon();
+                        break;
+                }
+               
+            }
+        }
+
+        public void NumaraGuncelle()
+        {
+            Console.WriteLine("Numarasını güncellemek istediğiniz kişinin adını ve soyadını giriniz:");
+            string input = Console.ReadLine();
+            if (telefonRehberi.ContainsKey(input))
+            {
+                Console.WriteLine("Yeni numarayı giriniz:");
+                long newNumber = long.Parse(Console.ReadLine());
+                long degistir = telefonRehberi[input];
+                telefonRehberi[input] = newNumber;
+                Console.WriteLine("{0} numarası {1} ile değiştirildi.\nMenuye donmek için bir tuşa basın.",degistir,newNumber);
+                MenuyeDon();
+
+            }
+            else
+            {
+                Console.WriteLine(" Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.\n" +
+                        "* Güncellemeyi sonlandırmak için    : (1)\n" +
+                        "* Yeniden denemek için              : (2)");
+                int cevap = int.Parse(Console.ReadLine());
+                switch (cevap)
+                {
+                    case 1:
+                        Console.Clear();
+                        Program.Menu();
+                        break;
+                    case 2:
+                        NumaraGuncelle();
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış sayı girişi");
+                        MenuyeDon();
+                        break;
+                }
+            }
+        }
+
+        public void Arama()
+        {
+            Console.WriteLine("Arama yapmak istediğiniz tipi seçiniz.\n" +
+                "**********************************************\n" +
+                "İsim veya soyisime göre arama yapmak için: (1)\n" +
+                "Telefon numarasına göre arama yapmak için: (2)");
+
+            int cevap = int.Parse(Console.ReadLine());
+            switch (cevap)
+            {
+                case 1:
+                    Console.WriteLine("Aramak istediğiniz kişinin isim veya soyismini giriniz:");
+                    IsmeGöreArama(Console.ReadLine());
+                    break;
+                case 2:
+                    Console.WriteLine("Aramak istediğiniz kişinin numarasını giriniz:");
+                    NumarayaGöreArama(long.Parse(Console.ReadLine()));
+                    break;
+                default:
+                    Console.WriteLine("Yanlış sayı girişi");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Program.Menu();
+                    break;
+            }
+        }
+
+        public void IsmeGöreArama(string input)
+        {
+            if (telefonRehberi.ContainsKey(input))
+            {
+                Console.WriteLine("*****Arama Sonuçları*****");
+                Console.WriteLine("İsim-soyisim: {0}",input);
+                Console.WriteLine("Telefon numarası: {0}",telefonRehberi[input]);
+                Console.WriteLine("-------");
+                Console.WriteLine("Menuye donmek için bir tuşa basın.");
+                MenuyeDon();
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz bilgilerde biri rehberde bulunamadı.\nMenuye dönmek için bir tuşa basın.");
+                MenuyeDon();
+            }
+        }
+
+        public void NumarayaGöreArama(long input)
+        {
+            if (telefonRehberi.ContainsValue(input))
+            {
+                Console.WriteLine("*****Arama Sonuçlarınız*****");
+                string key = "";
+                foreach(var user in telefonRehberi)
+                {
+                    if (user.Value == input)
+                    {
+                        key = user.Key;
+                    }
+                }
+                Console.WriteLine("İsim-soyisim: {0}", key);
+                Console.WriteLine("Telefon numarası: {0}",input);
+                Console.WriteLine("Menuye donmek için bir tuşa basın.");
+                MenuyeDon();
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz bilgilerde biri rehberde bulunamadı.\nMenuye dönmek için bir tuşa basın.");
+                MenuyeDon();
+            }
+
+        }
+
+
+
+        public void RehberiListele()
+        {
+            Console.WriteLine("Telefon Rehberi");
+            Console.WriteLine("*********************************");
+            foreach(var user in telefonRehberi)
+            {
+                Console.WriteLine("İsim-Soyisim: {0}",user.Key);
+                Console.WriteLine("Telefon numarası: {0}",user.Value);
+                Console.WriteLine("----------------------------");
+            }
+
+            Console.WriteLine("Menuye donmek için bir tuşa basın.");
+            Console.ReadKey();
+            Console.Clear();
+            Program.Menu();
+        }
+
+
+        private void MenuyeDon()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Program.Menu();
+        }
+
+
+
+    }
+}
---
+++ TelefonRehberi/TelefonRehberi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TelefonRehberi
+{
+    class Program:KisiIslemleri
+    {
+        static KisiIslemleri islemler = new();
+        static void Main(string[] args)
+        {
+
+            islemler.telefonRehberi.Add("Ayşe Yilmaz", 05431475689);
+            islemler.telefonRehberi.Add("Ahmet Yıldırım",05463218947);
+            islemler.telefonRehberi.Add("Ezgi Karayel",05346512871);
+            islemler.telefonRehberi.Add("Kaan Demir",05312347896);
+            islemler.telefonRehberi.Add("Melisa Çelik",05412368452);
+
+            Menu();
+
+
+
+
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz : \n" +
+               "******************************************\n" +
+               "(1) Yeni Numara Kaydetmek\n" +
+               "(2) Varolan Numarayı Silmek\n" +
+               "(3) Varolan Numarayı Güncelleme\n" +
+               "(4) Rehberi Listeleme\n" +
+               "(5) Rehberde Arama Yapmak");
+
+            Console.WriteLine("Yapmak istediğiniz işlemin numarasını yazınız:");
+            int islemNumarasi = Convert.ToInt32(Console.ReadLine());
+
+            switch (islemNumarasi)
+            {
+                case 1:
+                    Console.WriteLine("Yeni numara ekleme");
+                    islemler.KayitEkle();
+                    break;
+                case 2:
+                    islemler.NumaraSil();
+                    break;
+                case 3:
+                    islemler.NumaraGuncelle();
+                    break;
+                case 4:
+                    islemler.RehberiListele();
+                    break;
+                case 5:
+                    islemler.Arama();
+                    break;
+                default:
+                    Console.WriteLine("Yanlış sayı girişi");
+                    break;
+            }
+        }
+
+    }
+}
